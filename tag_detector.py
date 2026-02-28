6d0a5feefe5970f02e6cdfb4b54e18cc0b7b248e
nekiwo <58634064+nekiwo@users.noreply.github.com>
2026-02-28 01:23:50 -0600

    pose estimation in progress

--- tag_detector.py
+++ tag_detector.py
@@ -5,6 +5,12 @@ class TagDetector:
     detector_params = cv.aruco.DetectorParameters()
     dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_APRILTAG_36h11)
     aruco_detector = None
+    obj_points = np.float32([
+            [-0.5, 0.5, 0],
+            [0.5, 0.5, 0],
+            [0.5, -0.5, 0],
+            [-0.5, -0.5, 0]
+        ])
 
     def __init__(self):
         self.detector_params.cornerRefinementMethod = cv.aruco.CORNER_REFINE_APRILTAG
@@ -17,4 +23,5 @@ class TagDetector:
         return ids, corners
     
     def estimate_pose(self, corners, camera_mat, dist_coeffs):
-        pass
+        ret, rvecs, tvecs = cv.solvePnP(self.obj_points, corners, camera_mat, dist_coeffs, flags=cv.SOLVEPNP_IPPE_SQUARE)
+        return 0
