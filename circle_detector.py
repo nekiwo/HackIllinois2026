2a5a0cc0fb733dbaed94ce2da112ad45db757a4c
Neel Poluri <mightymickey81@gmail.com>
2026-02-28 21:29:24 -0600

    iterated circle detection

--- circle_detector.py
+++ circle_detector.py
@@ -1,19 +1,32 @@
 import cv2 as cv
 import numpy as np
 
+MIN_RADIUS=1
+MAX_RADIUS=20
+
 class CircleDetector:
-    def detect(self, gray):
-        rows = gray.shape[0]
-        circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 8,
-                               param1=100, param2=5,
-                               minRadius=1, maxRadius=100)
-        return circles
+    def detect(self, gray, num_iterations=4):
+        all_circles = np.empty(1)
+        group_size = (MAX_RADIUS- MIN_RADIUS) / num_iterations
+        for i in range(num_iterations):
+            rows = gray.shape[0]
+            circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 16,
+                                param1=100, param2=20, minRadius=int(group_size*i + MIN_RADIUS), 
+                                maxRadius=int(group_size*(i+1) + MIN_RADIUS))
+            if circles is None:
+                continue
+            if all_circles.ndim < 2:
+                all_circles = circles
+            else:
+                np.concatenate((all_circles, circles), axis=1)
+        if all_circles.ndim <2:
+            return None
+        return all_circles
     
     def draw(self, frame, circles):
         if circles is not None:
             circles = np.uint16(np.around(circles))
             for i in circles[0, :]:
-                print(i)
                 center = (i[0], i[1])
                 cv.circle(frame, center, 1, (0, 100, 100), 3)
                 radius = i[2]
